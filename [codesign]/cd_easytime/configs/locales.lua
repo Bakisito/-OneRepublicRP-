29212c510d6e4e500901aa58054bb4a172c4305b
Bakisito <73045761+Bakisito@users.noreply.github.com>
2023-07-24 10:59:41 -0500

    Added

---
+++ [codesign]/cd_easytime/configs/locales.lua
@@ -0,0 +1,16 @@
+Locales = {
+    ['EN'] = {
+        ['invalid_permissions'] = 'You do not have permissions to use this command',
+        ['drop_player'] = 'Did you know, that banging your head against a wall for one hour burns 150 calories. Alternatively, you can walk your dog for 45 minutes, which also burns 150 calories – and is much less painful. Dont cheat.',
+    },
+
+    ['FR'] = {
+        ['invalid_permissions'] = 'Vous ne disposez pas des autorisations nécessaires pour utiliser cette commande',
+        ['drop_player'] = 'Did you know, that banging your head against a wall for one hour burns 150 calories. Alternatively, you can walk your dog for 45 minutes, which also burns 150 calories – and is much less painful. Dont cheat.',
+    },
+
+    ['ES'] = {
+        ['invalid_permissions'] = 'No tienes permisos para usar este comando',
+        ['drop_player'] = '¿Sabías que golpearte la cabeza contra una pared durante una hora quema 150 calorías? También puedes pasear a tu perro durante 45 minutos, que también quema 150 calorías, y es mucho menos doloroso. No hagas trampas',
+    },
+}
