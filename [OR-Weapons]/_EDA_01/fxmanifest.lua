2fc428689a46b995e0c5773c6aa62d26972e91f4
Bakisito <73045761+Bakisito@users.noreply.github.com>
2023-08-09 19:09:18 -0500

    Actualizado DriveBy
    Se agregaron nuevos layouts para vehiculos japoneses.

--- [OR-Weapons]/_EDA_01/fxmanifest.lua
+++ [OR-Weapons]/_EDA_01/fxmanifest.lua
@@ -2,7 +2,7 @@
 game 'gta5'
 fx_version 'cerulean'
 
-version '1.0.0'
+version '1.1.0'
 author 'Delta District'
 
 files
@@ -18,4 +18,6 @@ escrow_ignore {
 
 data_file 'CLIP_SETS_FILE' 'data/clip_set.xml'
 data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+dependency '/assetpacks'
+
 dependency '/assetpacks'
